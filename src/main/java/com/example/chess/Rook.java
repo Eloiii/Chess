01be7cb0216b3642eacff86300078a207bbc4830
= <eloi.charra@gmail.com>
2021-11-01 22:22:13 +0100

    Fix rook movments

--- src/main/java/com/example/chess/Rook.java
+++ src/main/java/com/example/chess/Rook.java
@@ -14,29 +14,40 @@ public class Rook implements Piece {
     public ArrayList<Cell> getLegalMoves(int rowFrom, int colFrom, Board board) {
         ArrayList<Cell> results = new ArrayList<>();
 
-        for (int rowFor = rowFrom; rowFor < BoardDimensions.MAX_ROW.getValue(); rowFor++) {
+        for (int rowFor = rowFrom + 1; rowFor < BoardDimensions.MAX_ROW.getValue(); rowFor++) {
             Piece piece = board.at(rowFor, colFrom).getPiece();
-            if (!piece.isVoidPiece() && piece.getColor() == this.color)
+            if (!piece.isVoidPiece()) {
+                if (piece.getColor() != this.color)
+                    results.add(new Cell(rowFor, colFrom));
                 break;
+            }
             results.add(new Cell(rowFor, colFrom));
         }
-        for (int rowFor = 0; rowFor < rowFrom; rowFor++) {
+        for (int rowFor = rowFrom - 1; rowFor > -1; rowFor--) {
             Piece piece = board.at(rowFor, colFrom).getPiece();
-            if (!piece.isVoidPiece() && piece.getColor() == this.color)
+            if (!piece.isVoidPiece()) {
+                if (piece.getColor() != this.color)
+                    results.add(new Cell(rowFor, colFrom));
                 break;
+            }
             results.add(new Cell(rowFor, colFrom));
         }
-
-        for (int colFor = colFrom; colFor < BoardDimensions.MAX_COL.getValue(); colFor++) {
+        for (int colFor = colFrom + 1; colFor < BoardDimensions.MAX_COL.getValue(); colFor++) {
             Piece piece = board.at(rowFrom, colFor).getPiece();
-            if (!piece.isVoidPiece() && piece.getColor() == this.color)
+            if (!piece.isVoidPiece()) {
+                if (piece.getColor() != this.color)
+                    results.add(new Cell(rowFrom, colFor));
                 break;
+            }
             results.add(new Cell(rowFrom, colFor));
         }
-        for (int colFor = 0; colFor < colFrom; colFor++) {
+        for (int colFor = rowFrom - 1; colFor > -1; colFor--) {
             Piece piece = board.at(rowFrom, colFor).getPiece();
-            if (!piece.isVoidPiece() && piece.getColor() == this.color)
+            if (!piece.isVoidPiece()) {
+                if (piece.getColor() != this.color)
+                    results.add(new Cell(rowFrom, colFor));
                 break;
+            }
             results.add(new Cell(rowFrom, colFor));
         }
         return results;
